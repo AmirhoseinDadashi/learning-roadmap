c8a397d661b40cfb35082e22579ec9d59240cd7b
AmirhoseinDadashi <amir.h.dadashi110@gmail.com>
2025-08-21 16:49:01 +0330

    First project added

---
+++ day2.py
@@ -0,0 +1,7 @@
+name = input("pleas your name:")
+a = int(input("please your age:"))
+print("your name is:", name)
+if a>= 18:
+    print("welcome", name)
+else:
+    print("sorry your age is not allowed")
